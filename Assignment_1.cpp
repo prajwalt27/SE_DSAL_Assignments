de3d9447a674ab26ef1444f4dae6272b5ffaac00
Prajwal Toundakar <81010237+prajwalt27@users.noreply.github.com>
2022-02-24 23:14:29 +0530

    Update Assignment_1.cpp

--- Assignment_1.cpp
+++ Assignment_1.cpp
@@ -1,434 +1,387 @@
-#include<iostream>
-#include<queue>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <stack>
+#define max 1000
 using namespace std;
-
-template <class T>
-class Stack;
-class BinaryTree;
-
-template <class T>
-class SNode
-{
-    T data;
-    SNode<T> *next;
-
-public:
-    SNode<T>()
-    {
-        data = 0;
-        next = NULL;
-    }
-
-    SNode<T>(T val)
-    {
-        data = val;
-        next = NULL;
-    }
-
-    friend class Stack<T>;
-};
-
-template <class T>
-class Stack
-{
-    SNode<T> *top;
-
-public:
-    friend class BinaryTree;
-    Stack<T>()
-    {
-        top = NULL;
-    }
-
-    void push(T val)
-    {
-        SNode<T> *temp = new SNode<T>(val);
-        if (top == NULL)
-        {
-            top = temp;
-            return;
-        }
-        temp->next = top;
-        top = temp;
-    }
-
-    void pop()
-    {
-        if (!isEmpty())
-        {
-            SNode<T> *todelete = top;
-            top = top->next;
-            delete todelete;
-        }
-        else
-        {
-            cout << "Stack underflow" << endl;
-        }
-    }
-
-    bool isEmpty()
-    {
-        return top == NULL;
-    }
-
-    T Top()
-    {
-        return top->data;
-    }
-};
-
-
-
-template <class T>
-class Queue;
-
-template <class T>
-class QNode
-{
-    T data;
-    QNode<T>* next;
-    public:
-        QNode<T>()
-        {
-            data=0;
-            next=NULL;
-        }
-
-        QNode<T>(T val){
-            data=val;
-            next=NULL;
-        }
-
-    friend class Queue<T>;
-};
-
-template <class T>
-class Queue
-{
-    QNode<T>*front,*rear;
-
-    public:
-    friend class BinaryTree;
-        Queue<T>()
-        {
-            front=rear=NULL;
-        }
-
-        QNode<T>* getfront(){
-            return front;
-        }
-
-        void push(T val){
-            QNode<T> *temp = new QNode<T>(val);
-
-            if(front==NULL){
-                front=rear=temp;
-                return;
-            }
-            rear->next = temp;
-            rear = temp;
-        }
-
-        void pop(){
-            if(!empty()){
-                QNode<T>* todelete = front;
-                front = front->next;
-                delete todelete;
-            }
-            else{
-                cout<<"Queue underflow"<<endl;
-            }
-        }
-
-        bool empty(){
-            return front==NULL;
-        }
-};
 
 class Node
 {
-    int data;
-    Node *left, *right;
-
 public:
-    Node() {}
-    Node(int d)
-    {
-        data = d;
-        left = NULL;
-        right = NULL;
-    }
-    friend class BinaryTree;
+    int data;
+    Node *left;
+    Node *right;
 };
 
-class BinaryTree
+class bTree
 {
-    Node *root;
-
 public:
-    BinaryTree()
-    {
-        root = NULL;
-    }
 
-    Node *get_root()
+   //Operations
+    Node *create();
+
+    //Recursive traversals
+    void inorder_r(Node *root);
+    void preorder_r(Node *root);
+    void postorder_r(Node *root);
+
+   //iterative traversals
+    void I_inorder(Node *root);
+    void I_preorder(Node *root);
+    void I_postorder(Node *root);
+
+    void mirror(Node *root);
+
+    int height(Node *root);
+
+    int getLeafCount(Node *root);
+
+    int countInternalNodes(Node *root);
+
+    void deleteTree(Node *root);
+};
+
+
+Node *bTree::create()
+{
+    int key;
+    Node *root;
+    bool flag = false;
+    root = new Node();
+    cout << "\n1.NULL \n2.Input \n";
+    cin >> key;
+    if (key == 1)
     {
+        return 0;
+    }
+    else if ( key == 2)
+    {
+
+        cout << "Enter element \n";
+        cin >> root->data;
+        cout << "Enter element to left of " << root->data;
+        root->left = create();
+        cout << "Enter element to right of " << root->data;
+       root->right = create();
+        flag=true;
         return root;
     }
-
-    Node *create()
+    else
     {
-        int val;
-        cout << "enter data(-1 if no child)=";
-        cin >> val;
-        if (val == -1)
-            return 0;
-        Node *newnode = new Node(val);
-        if (root == NULL)
-            root = newnode;
-        cout << "left child of " << val << endl;
-        newnode->left = create();
-        cout << "right child of " << val << endl;
-        newnode->right = create();
-
-        return newnode;
+        cout << "Enter valid choice..!"<< "\n";
+        flag=false;
+        return 0;
     }
+}
 
-    void create2(Node *newnode)
-    {
-        if (root == NULL)
-        {
-            root = newnode;
-            cout << "\nRoot Created Successfully" << endl;
-        }
-        else
-        {
-            Node *temp = root; // Traversing Node
-            while (true)
-            {
-                int option;
-                cout << "\nYou are at node: " << temp->data << endl;
-                cout << "\n1.Go to left\t 2.Go to right\t 5.Exit" << endl;
-                cin >> option;
-                if (option == 1)
-                {
-                    if (temp->left == NULL)
-                    {
-                        temp->left = newnode;
-                        cout << newnode->data << " is left child of node " << temp->data << endl;
-                        break;
-                    }
-                    else
-                        temp = temp->left;
-                }
-                else if (option == 2)
-                {
-                    if (temp->right == NULL)
-                    {
-                        temp->right = newnode;
-                        cout << newnode->data << " is right child of " << temp->data << endl;
-                        break;
-                    }
-                    else
-                        temp = temp->right;
-                }
-            }
-        }
-    }
 
-    void inorder(Node *temp)
-    {
-        if (temp == NULL)
-            return;
-        inorder(temp->left);
-        cout << temp->data << "  ";
-        inorder(temp->right);
-    }
+//Recursive Traversals 
 
-    void preorder(Node *temp)
-    {
-        if (temp == NULL)
-        {
-            return;
-        }
-
-        cout << temp->data << "   ";
-        preorder(temp->left);
-        preorder(temp->right);
-    }
-
-    void postorder(Node *temp)
-    {
-        if (temp == NULL)
-            return;
-        postorder(temp->left);
-        postorder(temp->right);
-        cout << temp->data << "   ";
-    }
-
-    void inor()
-    {
-        Stack<Node *> st;
-        Node *temp = root;
-        while (1)
-        {
-            if (temp != NULL)
-            {
-                st.push(temp);
-                temp = temp->left;
-            }
-
-            else
-            {
-                if (st.isEmpty())
-                    break;
-                cout << st.Top()->data << "  ";
-                temp = st.Top()->right;
-                st.pop();
-                
-
-                // temp = temp->right;
-            }
-        }
-    }
-
-    void preo()
-    {
-        Node *temp = root;
-        Stack<Node *> st;
-        while (1)
-        {
-            if (temp != NULL)
-            {
-                cout << temp->data << "  ";
-                st.push(temp);
-                temp = temp->left;
-            }
-            else
-            {
-                if (st.isEmpty())
-                    break;
-                temp = st.Top()->right;
-                st.pop();
-            }
-        }
-    }
-
-//Recursive 
-    int height(Node *current)
-    {
-        if (current == NULL)
-            return 0;
-        int lh = height(current->left);
-        int rh = height(current->right);
-        return 1 + max(lh, rh);
-    }
-//Non-recursive
-    int height2(Node *current)
-    {
-        if (current == NULL)
-            return 0;
-        else if (current->right == NULL && current->left == NULL)
-            return 1;
-        else if (current->right == NULL)
-        {
-            return 1 + height2(current->left);
-        }
-        else if (current->left == NULL)
-        {
-            return 1 + height2(current->right);
-        }
-        else
-        {
-            int l = 1 + height2(current->left);
-            int r = 1 + height2(current->right);
-
-            return max(l, r);
-        }
-    }
- 
-    void mirror()
-    {
-        // Queue<Node*> q;
-        queue<Node*>q;
-        q.push(root);
-        while (!q.empty())
-        {
-            Node*front =q.front();
-            q.pop();
-
-            if (front->left != NULL && front->right != NULL)
-            {
-                q.push(front->right);
-                q.push(front->left);
-            }
-            else if (front->right != NULL)
-            {
-                q.push(front->right);
-            }
-            else if (front->left != NULL)
-            {
-                q.push(front->left);
-            }
-
-           //or swap only data
-            Node*temp=front->right;
-            front->right=front->left;
-            front->left=temp;
-        }
-    }
-
-};
-
-int main(int argc, char const *argv[])
-
+void bTree::inorder_r(Node *root)
 {
-    BinaryTree b;
-    int choice;
-    while(1)
-  {  cout<<"\nMenu\n1:create\n2:traversal\n3:height  "<<"\n";
-    cout<<"Enter your choice: ";
-    cin>>choice;
+    if (root == NULL)
+        return;
 
-    switch (choice)
+    inorder_r(root->left);
+    cout << root->data << " ";
+    inorder_r(root->right);
+}
+
+void bTree::preorder_r(Node *root)
+{
+
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder_r(root->left);
+    preorder_r(root->right);
+}
+
+void bTree::postorder_r(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    postorder_r(root->left);
+    postorder_r(root->right);
+    cout << root->data << " ";
+}
+
+
+//Inorder Traversals
+void bTree::I_inorder(Node *root)
+{
+    Node *d;
+    int finish, top;
+    top = 0;
+    d = root;
+    finish = 0;
+    Node *stack[max];
+    do
     {
-    case 1:
-        cout<<"\n1:recursive way\n2:non recursive way=";
-        cin>>choice;
-        if(choice==1)  Node*root=b.create();
+        while (d != NULL)
+        {
+            top++;
+            stack[top] = d;
+            d = d->left;
+        }
+        if (top != 0)
+        {
+            d = stack[top];
+            top--;
+            cout << d->data << " ";
+            d = d->right;
+        }
+        else
+            finish = 1;
+    } while (finish == 0);
+}
+
+void bTree::I_preorder(Node *root)
+{
+    Node *d;
+    int finish, top;
+    top = 0;
+    d = root;
+    finish = 0;
+    Node *stack[max];
+    do
+    {
+        while (d != NULL)
+        {
+            top++;
+            stack[top] = d;
+            cout << d->data << " ";
+            d = d->left;
+        }
+
+        if (top != 0)
+        {
+            d = stack[top];
+            top--;
+            d = d->right;
+        }
+        else
+            finish = 1;
+
+    } while (finish == 0);
+}
+
+void bTree::I_postorder(Node *root)
+{
+    int top = -1;
+    Node *p = root;
+    Node *stack[max];
+    do
+    {
+        while (p != NULL)
+        {
+            if (p->right != NULL)
+            {
+                top++;
+                stack[top] = p->right;
+            }
+            top++;
+            stack[top] = p;
+            p = p->left;
+        }
+
+        p = stack[top];
+        top--;
+        if (p->right && (stack[top] == p->right))
+        {
+            stack[top] = p;
+            p = p->right;
+        }
         else
         {
-            while (1)
-            {
-                 cout<<"\nenter data=(-1 for no node)=";int val;cin>>val;
-                 if(val==-1)  break;
-        Node *newnode = new Node(val);
-        b.create2(newnode);
-            }  
+            cout << p->data << " ";
+            p = NULL;
         }
-        break;
+    } while (top != -1);
+}
 
-    case 2:
-        cout<<"1:preorder recursive\t2:preorder non recursive\n";
-        cout<<"3:inorder recursive\t4:inorder non recursive\n";
-        cout<<"5:postorder recursive\t6:postorder non recursive\n";
+int bTree::height(Node *root)
+{
+    Node *p = root;
+    if (p == NULL)
+        return 0;
+    else
+    {
+        int left;
+        int right;
+        left = height(p->left);
+        right = height(p->right);
+        if (left> right)
+        {
+            return left + 1;
+        }
+        else
+            {
+                return right + 1;
+            }
+    }
+}
 
-        cin>>choice;
-        if(choice==1) b.preorder(b.get_root());
-        else  if(choice==2) b.preo();
-        else if(choice==3) b.inorder(b.get_root());
-        else if(choice==4)  b.inor();
-        else if(choice==5)  b.postorder(b.get_root());
-        break;
+void bTree::mirror(Node *root)
+{
+    Node *p = root;
+    if (p == NULL)
+        return;
+    else
+    {
+        Node *swap;
+
+        mirror(p->left);
+        mirror(p->right);
+        swap = p->left;
+        p->left = p->right;
+        p->right = swap;
+    }
+}
+
+int bTree::getLeafCount(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "\nTree is empty" << endl;
+        return 0;
+    }
+    if (root->left == NULL && root->right == NULL)
+        return 1;
+    else
+        return getLeafCount(root->left) + getLeafCount(root->right);
+}
+
+int bTree::countInternalNodes(Node *root)
+{
+
+    if (root != NULL)
+    {
         
-    case 3:
-    cout<<"\nheight of tree is= "<<b.height(b.get_root())<<endl;
-    break;
+        int result = countInternalNodes(root->left) + countInternalNodes(root->right);
+        if (root->left != NULL || root->right != NULL)
+        {
 
-    
-    default:
-        break;
-    }}
+            return result + 1;
+        }
+        else
+        {
+
+            return result;
+        }
+    }
+    else
+    {
+
+        cout << "\nTree is empty" << endl;
+        return 0;
+    }
+}
+
+void bTree::deleteTree(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+
+    deleteTree(root->left);
+
+    deleteTree(root->right);
+
+    delete root;
+    root = NULL;
+}
+
+int main()
+{
+    char input = 'y';
+    while (input != 'n')
+    {
+        int choice;
+        Node *n;
+        bTree *op;
+        cout << "*-------Main Menu--------*"<< "\n";
+        cout << "\n1.Create tree\n2.Recursive Traversal\n3.Iterative Traversal\n4.Height of tree\n5.Mirroing of tree\n6.Leaf count\n7.Internal node count\n8.Delete the tree";
+        cout << "\n\nEnter your choice :";
+        cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            n = op->create();
+            
+            cout << "Created successfully !" << endl;
+            break;
+        case 2:
+            if (n != NULL)
+            {
+                cout << "\n<---------Recursive Traversal---------->";
+                cout << "\n Inorder traversal  :";
+                op->inorder_r(n);
+                cout << "\n Preorder traversal  :";
+                op->preorder_r(n);
+                cout << "\n Postorder traversal  :";
+                op->postorder_r(n);
+                break;
+            }
+            else
+            {
+                cout << "\nBinary tree is empty !";
+                break;
+            }
+
+        case 3:
+            if (n != NULL)
+            {
+                cout << "\n\n\n<----------Iterative Traversal------------>";
+                cout << "\n Inorder traversal  :";
+                op->I_inorder(n);
+                cout << "\n Preorder traversal  :";
+                op->I_preorder(n);
+                cout << "\n Postorder traversal  :";
+                op->I_postorder(n);
+                break;
+            }
+            else
+            {
+                cout << "\nBinary tree is empty";
+                break;
+            }
+
+        case 4:
+
+            cout << "\n\n Height of Binary tree : " << op->height(n) << endl;
+            break;
+
+        case 5:
+            cout << "original tree is :";
+            op->inorder_r(n);
+            cout << "\n\nmirror of original tree: ";
+            op->mirror(n);
+            op->inorder_r(n);
+
+            break;
+
+        case 6:
+            cout << "\n\nThe count of leaf node is:" << op->getLeafCount(n);
+            break;
+
+        case 7:
+            cout << "\n\nThe count of Internal node is:" << op->countInternalNodes(n);
+            break;
+        case 8:
+            op->deleteTree(n);
+            cout << "\n\nTree deleted successfully." << endl;
+            n = NULL;
+            break;
+
+        default:
+            cout << "Enter valid choice";
+            break;
+        }
+        cout << "\n\nDo you want to perform again (y/n) :";
+        cin >> input;
+    }
+
     return 0;
 }
